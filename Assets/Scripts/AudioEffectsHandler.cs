98673acca52f742a01f06781ceac80a3acb6e970
Alan-mag <alanmagelssen@gmail.com>
2021-07-05 20:29:42 -0700

    add test scene practice 1

---
+++ Assets/Scripts/AudioEffectsHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffectsHandler : MonoBehaviour
+{
+    public SoundAudioClip[] soundAudioClips;
+
+    static SoundAudioClip[] soundAudioClipsArray;
+
+    private void Awake() => soundAudioClipsArray = soundAudioClips;
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public EffectsSounds sound;
+        public SoundAudioClip audioClip;
+    }
+
+    public static SoundAudioClip GetAudioClip(EffectsSounds sound)
+    {
+        foreach(SoundAudioClip soundAudioClip in soundAudioClipsArray)
+        {
+            if (soundAudioClip.sound == sound)
+                return soundAudioClip.audioClip;
+        }
+        return null;
+    }
+}
+
+// TODO: update with what's needed
+public enum EffectsSounds
+{
+    PlayerLaser_Lv1,
+    LaserHit
+}
